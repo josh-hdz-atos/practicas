c64e2949c6ed415c1298e2657d892c0043317553
Joshua Hernandez <joshua-salvador.hernandezreza.external@atos.net>
2022-09-01 12:41:57 -0500

    start performance review

---
+++ PerformanceReview/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PerformanceReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz();
+        }
+
+        static void FizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write($"{i}: ");
+                if (i % 3 == 0) Console.Write("Fizz");
+                if (i % 5 == 0) Console.Write("Buzz");
+                Console.WriteLine("");
+            }
+        }
+
+        static void RemoveLowerCase(ref string str)
+        {
+            Regex wordStart = new Regex("")
+
+
+        }
+    }
+}
